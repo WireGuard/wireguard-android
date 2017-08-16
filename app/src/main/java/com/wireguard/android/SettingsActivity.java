08992185ecfe359ae7607a779b984fc6c3b8b22b
Samuel Holland <samuel@sholland.org>
2017-08-16 04:34:14 -0500

    SettingsActivity: Implement primary config setting
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- app/src/main/java/com/wireguard/android/SettingsActivity.java
+++ app/src/main/java/com/wireguard/android/SettingsActivity.java
@@ -1,6 +1,23 @@
 package com.wireguard.android;
 
 import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
 
 public class SettingsActivity extends Activity {
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(android.R.id.content, new SettingsFragment()).commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(final Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
 }
