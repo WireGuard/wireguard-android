d133ac2ab623f6daffba9f16d2712557d2a1b9d2
Jason A. Donenfeld <Jason@zx2c4.com>
2018-04-18 16:44:05 +0200

    Determine MTU automatically

--- app/tools/libwg-go/jni.c
+++ app/tools/libwg-go/jni.c
@@ -1,12 +1,12 @@
 #include <jni.h>
 
 struct go_string { const char *str; long n; };
-extern int wgTurnOn(struct go_string ifname, int tun_fd, int mtu, struct go_string settings);
+extern int wgTurnOn(struct go_string ifname, int tun_fd, struct go_string settings);
 extern void wgTurnOff(int handle);
 extern int wgGetSocketV4(int handle);
 extern int wgGetSocketV6(int handle);
 
-JNIEXPORT jint JNICALL Java_com_wireguard_android_backend_GoBackend_wgTurnOn(JNIEnv *env, jclass c, jstring ifname, jint tun_fd, jint mtu, jstring settings)
+JNIEXPORT jint JNICALL Java_com_wireguard_android_backend_GoBackend_wgTurnOn(JNIEnv *env, jclass c, jstring ifname, jint tun_fd, jstring settings)
 {
 	const char *ifname_str = (*env)->GetStringUTFChars(env, ifname, 0);
 	size_t ifname_len = (*env)->GetStringUTFLength(env, ifname);
@@ -15,7 +15,7 @@ JNIEXPORT jint JNICALL Java_com_wireguard_android_backend_GoBackend_wgTurnOn(JNI
 	int ret = wgTurnOn((struct go_string){
 		.str = ifname_str,
 		.n = ifname_len
-	}, tun_fd, mtu, (struct go_string){
+	}, tun_fd, (struct go_string){
 		.str = settings_str,
 		.n = settings_len
 	});
