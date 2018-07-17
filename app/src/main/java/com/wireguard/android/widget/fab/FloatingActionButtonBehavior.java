df0d2eb452de15a9f4b7078e379eabf34cba405b
Harsh Shandilya <harsh@prjkt.io>
2018-07-17 20:31:41 +0530

    fab: slide fab up when a snackbar shows
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

---
+++ app/src/main/java/com/wireguard/android/widget/fab/FloatingActionButtonBehavior.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2018 Harsh Shandilya <msfjarvis@gmail.com>
+ * Copyright © 2018 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.wireguard.android.widget.fab;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class FloatingActionButtonBehavior extends CoordinatorLayout.Behavior<FloatingActionsMenu> {
+
+    public FloatingActionButtonBehavior(final Context context, final AttributeSet attrs) { }
+
+    @Override
+    public boolean layoutDependsOn(final CoordinatorLayout parent, final FloatingActionsMenu child,
+                                   final View dependency) {
+        return dependency instanceof Snackbar.SnackbarLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(final CoordinatorLayout parent, final FloatingActionsMenu child,
+                                          final View dependency) {
+        final float translationY = Math.min(0, dependency.getTranslationY() - dependency.getHeight());
+        child.setTranslationY(translationY);
+        return true;
+    }
+}
