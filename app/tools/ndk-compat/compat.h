996cbb5f2bc4a6dc2a9168052efb730ade59f902
Jason A. Donenfeld <Jason@zx2c4.com>
2018-07-24 18:09:18 +0200

    tools: support ancient NDKs
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

---
+++ app/tools/ndk-compat/compat.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: BSD
+ *
+ * Copyright (C) 2015-2018 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.
+ *
+ */
+
+#if defined(__ANDROID_API__) && __ANDROID_API__ < 18
+#include <stdio.h>
+ssize_t getdelim(char **buf, size_t *bufsiz, int delimiter, FILE *fp);
+ssize_t getline(char **buf, size_t *bufsiz, FILE *fp);
+#endif
+
+#if defined(__ANDROID_API__) && __ANDROID_API__ < 24
+char *strchrnul(const char *s, int c);
+#endif
+
