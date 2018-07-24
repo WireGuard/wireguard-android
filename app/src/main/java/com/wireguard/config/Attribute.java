WireGuard/wireguard-android

commit 71c67aa24ae21067148e34556a21dd1d2a7e3471

config: Minor cleanup
- Stop implicitly assuming locales in String.format
- Cleanup method visibilities
- Improve uses of Integer methods
- Remove unused getToken method

Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/com/wireguard/config/Attribute.java
@@ -7,6 +7,7 @@ import java.lang.reflect.Method;
 import java.net.InetAddress;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -88,15 +89,11 @@ enum Attribute {
     }
 
     public String composeWith(final int value) {
-        return String.format("%s = %d%n", token, value);
+        return String.format(Locale.getDefault(), "%s = %d%n", token, value);
     }
 
     public <T> String composeWith(final List<T> value) {
         return String.format("%s = %s%n", token, listToString(value));
-    }
-
-    public String getToken() {
-        return token;
     }
 
     public String parse(final CharSequence line) {

app/src/main/java/com/wireguard/config/Peer.java
@@ -15,6 +15,7 @@ import java.net.URISyntaxException;
 import java.net.UnknownHostException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Represents the configuration for a WireGuard peer (a [Peer] block).
@@ -167,7 +168,7 @@ public class Peer implements Parcelable {
     private String getEndpointString() {
         if (endpoint == null)
             return null;
-        return String.format("%s:%d", endpoint.getHostString(), endpoint.getPort());
+        return String.format(Locale.getDefault(), "%s:%d", endpoint.getHostString(), endpoint.getPort());
     }
 
     public String getResolvedEndpointString() throws UnknownHostException {
@@ -178,8 +179,14 @@ public class Peer implements Parcelable {
         if (endpoint.isUnresolved())
             throw new UnknownHostException(endpoint.getHostString());
         if (endpoint.getAddress() instanceof Inet6Address)
-            return String.format("[%s]:%d", endpoint.getAddress().getHostAddress(), endpoint.getPort());
-        return String.format("%s:%d",  endpoint.getAddress().getHostAddress(), endpoint.getPort());
+            return String.format(Locale.getDefault(),
+                    "[%s]:%d",
+                    endpoint.getAddress().getHostAddress(),
+                    endpoint.getPort());
+        return String.format(Locale.getDefault(),
+                "%s:%d",
+                endpoint.getAddress().getHostAddress(),
+                endpoint.getPort());
     }
 
     public int getPersistentKeepalive() {
@@ -189,7 +196,7 @@ public class Peer implements Parcelable {
     private String getPersistentKeepaliveString() {
         if (persistentKeepalive == 0)
             return null;
-        return new Integer(persistentKeepalive).toString();
+        return Integer.valueOf(persistentKeepalive).toString();
     }
 
     public String getPreSharedKey() {

app/src/main/java/com/wireguard/config/IPCidr.java
@@ -6,10 +6,11 @@ import android.os.Parcelable;
 import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
+import java.util.Locale;
 
 public class IPCidr implements Parcelable {
-    InetAddress address;
-    int cidr;
+    private InetAddress address;
+    private int cidr;
 
 
     public static final Parcelable.Creator<IPCidr> CREATOR = new Parcelable.Creator<IPCidr>() {
@@ -24,7 +25,7 @@ public class IPCidr implements Parcelable {
         }
     };
 
-    public IPCidr(String in) {
+    IPCidr(String in) {
         parse(in);
     }
 
@@ -35,7 +36,7 @@ public class IPCidr implements Parcelable {
             try {
                 cidr = Integer.parseInt(in.substring(slash + 1), 10);
                 in = in.substring(0, slash);
-            } catch (Exception e) {
+            } catch (Exception ignored) {
             }
         }
         address = Attribute.parseIPString(in);
@@ -55,7 +56,7 @@ public class IPCidr implements Parcelable {
 
     @Override
     public String toString() {
-        return String.format("%s/%d", address.getHostAddress(), cidr);
+        return String.format(Locale.getDefault(), "%s/%d", address.getHostAddress(), cidr);
     }
 
     @Override
@@ -71,7 +72,7 @@ public class IPCidr implements Parcelable {
     private IPCidr(final Parcel in) {
         try {
             parse(in.readString());
-        } catch (Exception e) {
+        } catch (Exception ignored) {
         }
     }
 

app/src/main/java/com/wireguard/config/Interface.java
@@ -151,7 +151,7 @@ public class Interface implements Parcelable {
         for (int i = 0; i < dnsItems; ++i) {
             try {
                 dnsList.add(InetAddress.getByAddress(in.createByteArray()));
-            } catch (Exception e) {
+            } catch (Exception ignored) {
             }
         }
         listenPort = in.readInt();
@@ -198,7 +198,7 @@ public class Interface implements Parcelable {
     private String getListenPortString() {
         if (listenPort == 0)
             return null;
-        return new Integer(listenPort).toString();
+        return Integer.valueOf(listenPort).toString();
     }
 
     public int getMtu() {
@@ -208,7 +208,7 @@ public class Interface implements Parcelable {
     private String getMtuString() {
         if (mtu == 0)
             return null;
-        return new Integer(mtu).toString();
+        return Integer.toString(mtu);
     }
 
     public String getPrivateKey() {

commit 402472237e8f6100573e9c3d41965a2e17e79578

config: Remove Locale based string format
The configurations are supposed to be in a very specific
format which is not user-facing and hence doesn't have to
be adjusted for locale avoiding both the redundancy as well
as potential breakages in the configuration file format from
different locales.

Fixes: 71c67aa24ae2 ("config: Minor cleanup")
Reported-by: Samuel Holland <samuel@sholland.org>
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/java/com/wireguard/config/Attribute.java
@@ -10,7 +10,6 @@ import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -71,7 +70,7 @@ public enum Attribute {
     }
 
     public String composeWith(final int value) {
-        return String.format(Locale.getDefault(), "%s = %d%n", token, value);
+        return String.format("%s = %d%n", token, value);
     }
 
     public <T> String composeWith(final Iterable<T> value) {

app/src/main/java/com/wireguard/config/Peer.java
@@ -25,7 +25,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
 
 import java9.lang.Iterables;
 
@@ -72,7 +71,7 @@ public class Peer {
     private String getEndpointString() {
         if (endpoint == null)
             return null;
-        return String.format(Locale.getDefault(), "%s:%d", endpoint.getHostString(), endpoint.getPort());
+        return String.format("%s:%d", endpoint.getHostString(), endpoint.getPort());
     }
 
     public int getPersistentKeepalive() {
@@ -104,12 +103,10 @@ public class Peer {
         if (endpoint.isUnresolved())
             throw new UnknownHostException(endpoint.getHostString());
         if (endpoint.getAddress() instanceof Inet6Address)
-            return String.format(Locale.getDefault(),
-                    "[%s]:%d",
+            return String.format("[%s]:%d",
                     endpoint.getAddress().getHostAddress(),
                     endpoint.getPort());
-        return String.format(Locale.getDefault(),
-                "%s:%d",
+        return String.format("%s:%d",
                 endpoint.getAddress().getHostAddress(),
                 endpoint.getPort());
     }