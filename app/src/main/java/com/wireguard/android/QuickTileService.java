6efbf654053a9d5110448fe405a2494c2c4ddaa3
Harsh Shandilya <me@msfjarvis.dev>
2018-08-12 16:39:20 +0530

    Target SDK 28
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/com/wireguard/android/QuickTileService.java
+++ app/src/main/java/com/wireguard/android/QuickTileService.java
@@ -58,13 +58,10 @@ public class QuickTileService extends TileService {
         return ret;
     }
 
-    /* TODO: for Android P: Build.VERSION_CODES.P */
-    private static final int BUILD_VERSION_CODES_P = 28;
-
     @SuppressWarnings("deprecation")
     @Override
     public void onCreate() {
-        if (Build.VERSION.SDK_INT >= BUILD_VERSION_CODES_P) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             iconOff = iconOn = Icon.createWithResource(this, R.drawable.ic_tile);
             return;
         }
