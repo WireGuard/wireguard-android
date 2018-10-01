364032fe846128ce241e6fcc406fe583d05a2e9e
Jason A. Donenfeld <Jason@zx2c4.com>
2018-10-01 16:02:55 +0200

    Update gradle and external deps
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

---
+++ app/tools/libwg-go/src/git.zx2c4.com/wireguard-go/queueconstants.go
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0
+ *
+ * Copyright (C) 2017-2018 WireGuard LLC. All Rights Reserved.
+ */
+
+package main
+
+/* Reduce memory consumption for Android */
+
+const (
+        QueueOutboundSize          = 1024
+        QueueInboundSize           = 1024
+        QueueHandshakeSize         = 1024
+        MaxSegmentSize             = 2200
+        PreallocatedBuffersPerPool = 4096
+)
