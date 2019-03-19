a066e3b3805ae74f011d14300fbcda410d818452
Jason A. Donenfeld <Jason@zx2c4.com>
2019-03-19 00:53:14 -0600

    InetEndpoint: properly match IPv6 addresses
    The old one didn't account for trailing digits.
    
    Reported-by: Brandon Jackson <bjackson@napshome.net>

--- app/src/main/java/com/wireguard/config/InetEndpoint.java
+++ app/src/main/java/com/wireguard/config/InetEndpoint.java
@@ -26,7 +26,7 @@ import java9.util.Optional;
  * Instances of this class are externally immutable.
  */
 public final class InetEndpoint {
-    private static final Pattern BARE_IPV6 = Pattern.compile("^[^\\[]*:");
+    private static final Pattern BARE_IPV6 = Pattern.compile("^[^\\[\\]]*:[^\\[\\]]*");
     private static final Pattern FORBIDDEN_CHARACTERS = Pattern.compile("[/?#]");
 
     private final String host;
