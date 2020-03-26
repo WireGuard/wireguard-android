b2bbaf050cf4aa9bd8a3824afde5fb4ff675c9a4
Harsh Shandilya <me@msfjarvis.dev>
2020-03-26 14:16:23 +0530

    util: Start converting to Kotlin
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

---
+++ ui/src/main/java/com/wireguard/android/util/ExceptionLoggers.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright © 2017-2019 WireGuard LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.wireguard.android.util
+
+import android.util.Log
+import java9.util.function.BiConsumer
+
+/**
+ * Helpers for logging exceptions from asynchronous tasks. These can be passed to
+ * `CompletionStage.whenComplete()` at the end of an asynchronous future chain.
+ */
+enum class ExceptionLoggers(private val priority: Int) : BiConsumer<Any?, Throwable?> {
+    D(Log.DEBUG), E(Log.ERROR);
+
+    override fun accept(result: Any?, throwable: Throwable?) {
+        if (throwable != null)
+            Log.println(Log.ERROR, TAG, Log.getStackTraceString(throwable))
+        else if (priority <= Log.DEBUG)
+            Log.println(priority, TAG, "Future completed successfully")
+    }
+
+    companion object {
+        private val TAG = "WireGuard/" + ExceptionLoggers::class.java.simpleName
+    }
+}
