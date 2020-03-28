b9bc4b3f6737cf2b061b766ed891b7987e4fd778
Jason A. Donenfeld <Jason@zx2c4.com>
2020-03-28 13:50:11 -0600

    MonkeyedTextInputEditText: introduce a new horror

---
+++ ui/src/main/java/com/wireguard/android/widget/MonkeyedTextInputEditText.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2020 WireGuard LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.wireguard.android.widget
+
+import android.content.Context
+import android.text.Editable
+import android.text.SpannableStringBuilder
+import android.util.AttributeSet
+import com.google.android.material.R
+import com.google.android.material.textfield.TextInputEditText
+
+class MonkeyedTextInputEditText @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = R.attr.editTextStyle) : TextInputEditText(context, attrs, defStyleAttr) {
+    @Override
+    override fun getText(): Editable? {
+        val text = super.getText()
+        if (!text.isNullOrEmpty())
+            return text
+        /* We want this expression in TextInputLayout.java to be true:
+         *        final boolean hasText = editText != null && !TextUtils.isEmpty(editText.getText());
+         * But for everyone else it should return the real value, so we check the caller.
+         */
+        if (Thread.currentThread().stackTrace[3].methodName == "updateLabelState")
+            return SpannableStringBuilder(" ")
+        return text
+    }
+}
