444a86cc9f642373ea04a42966e16869dac8dd69
Jason A. Donenfeld <Jason@zx2c4.com>
2020-09-22 15:52:58 +0200

    tv: wire in stats
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- ui/src/main/java/com/wireguard/android/activity/TvMainActivity.kt
+++ ui/src/main/java/com/wireguard/android/activity/TvMainActivity.kt
@@ -7,9 +7,12 @@ package com.wireguard.android.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.forEach
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
 import com.wireguard.android.Application
 import com.wireguard.android.R
@@ -20,7 +23,9 @@ import com.wireguard.android.databinding.TvActivityBinding
 import com.wireguard.android.databinding.TvTunnelListItemBinding
 import com.wireguard.android.model.ObservableTunnel
 import com.wireguard.android.util.ErrorMessages
+import com.wireguard.android.util.QuantityFormatter
 import com.wireguard.android.util.TunnelImporter
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class TvMainActivity : AppCompatActivity() {
@@ -50,12 +55,15 @@ class TvMainActivity : AppCompatActivity() {
                 Toast.makeText(this@TvMainActivity, message, Toast.LENGTH_LONG).show()
                 Log.e(TAG, message, e)
             }
+            updateStats()
         }
     }
 
+    lateinit var binding: TvActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = TvActivityBinding.inflate(layoutInflater)
+        binding = TvActivityBinding.inflate(layoutInflater)
         lifecycleScope.launch { binding.tunnels = Application.getTunnelManager().getTunnels() }
         binding.rowConfigurationHandler = object : ObservableKeyedRecyclerViewAdapter.RowConfigurationHandler<TvTunnelListItemBinding, ObservableTunnel> {
             override fun onConfigureRow(binding: TvTunnelListItemBinding, item: ObservableTunnel, position: Int) {
@@ -79,6 +87,33 @@ class TvMainActivity : AppCompatActivity() {
         }
         binding.executePendingBindings()
         setContentView(binding.root)
+
+        lifecycleScope.launch {
+            while (true) {
+                updateStats()
+                delay(1000)
+            }
+        }
+    }
+
+    private suspend fun updateStats() {
+        binding.tunnelList.forEach { viewItem ->
+            val listItem = DataBindingUtil.findBinding<TvTunnelListItemBinding>(viewItem) ?: return@forEach
+            try {
+                val tunnel = listItem.item!!
+                if (tunnel.state != Tunnel.State.UP) {
+                    throw Exception()
+                }
+                val statistics = tunnel.getStatisticsAsync()
+                val rx = statistics.totalRx()
+                val tx = statistics.totalTx()
+                listItem.tunnelTransfer.text = getString(R.string.transfer_rx_tx, QuantityFormatter.formatBytes(rx), QuantityFormatter.formatBytes(tx))
+                listItem.tunnelTransfer.visibility = View.VISIBLE
+            } catch (_: Throwable) {
+                listItem.tunnelTransfer.visibility = View.GONE
+                listItem.tunnelTransfer.text = ""
+            }
+        }
     }
 
     companion object {
---
+++ ui/src/main/java/com/wireguard/android/util/QuantityFormatter.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright © 2020 WireGuard LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.wireguard.android.util
+
+import com.wireguard.android.Application
+import com.wireguard.android.R
+
+object QuantityFormatter {
+    fun formatBytes(bytes: Long): String {
+        val context = Application.get().applicationContext
+        return when {
+            bytes < 1024 -> context.getString(R.string.transfer_bytes, bytes)
+            bytes < 1024 * 1024 -> context.getString(R.string.transfer_kibibytes, bytes / 1024.0)
+            bytes < 1024 * 1024 * 1024 -> context.getString(R.string.transfer_mibibytes, bytes / (1024.0 * 1024.0))
+            bytes < 1024 * 1024 * 1024 * 1024L -> context.getString(R.string.transfer_gibibytes, bytes / (1024.0 * 1024.0 * 1024.0))
+            else -> context.getString(R.string.transfer_tibibytes, bytes / (1024.0 * 1024.0 * 1024.0) / 1024.0)
+        }
+    }
+}
