745bb8a8baee2c46b49c11a910cba81f02a317d0
Andrey Butcitcyn <abutcitcyn@os33.com>
2022-01-18 14:31:06 +0300

    add export JAVA_HOME

--- build_tunnel.sh
+++ build_tunnel.sh
@@ -1,1 +1,2 @@
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/microsoft-11.jdk/Contents/Home
 ./gradlew :tunnel:assembleRelease -Dorg.gradle.java.home="/Applications/Android Studio.app/Contents/jre/Contents/Home"
