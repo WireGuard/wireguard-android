caf2cded6edf087c55bce873e649388daf234724
Jason A. Donenfeld <Jason@zx2c4.com>
2023-03-23 14:31:19 +0100

    ui: simplify IME displaying in ConfigNamingDialogFragment
    Also cleanup some event listener flow.
    
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- ui/src/main/java/com/wireguard/android/fragment/ConfigNamingDialogFragment.kt
+++ ui/src/main/java/com/wireguard/android/fragment/ConfigNamingDialogFragment.kt
@@ -5,15 +5,11 @@
 package com.wireguard.android.fragment
 
 import android.app.Dialog
-import android.content.DialogInterface
 import android.os.Bundle
-import android.view.inputmethod.InputMethodManager
-import androidx.appcompat.app.AlertDialog
-import androidx.core.content.getSystemService
+import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.textfield.TextInputEditText
 import com.wireguard.android.Application
 import com.wireguard.android.R
 import com.wireguard.android.databinding.ConfigNamingDialogFragmentBinding
@@ -27,7 +23,6 @@ import java.nio.charset.StandardCharsets
 class ConfigNamingDialogFragment : DialogFragment() {
     private var binding: ConfigNamingDialogFragmentBinding? = null
     private var config: Config? = null
-    private var imm: InputMethodManager? = null
 
     private fun createTunnelAndDismiss() {
         val binding = binding ?: return
@@ -42,12 +37,6 @@ class ConfigNamingDialogFragment : DialogFragment() {
             }
         }
     }
-
-    override fun dismiss() {
-        setKeyboardVisible(false)
-        super.dismiss()
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val configText = requireArguments().getString(KEY_CONFIG_TEXT)
@@ -64,7 +53,6 @@ class ConfigNamingDialogFragment : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val activity = requireActivity()
-        imm = activity.getSystemService()
         val alertDialogBuilder = MaterialAlertDialogBuilder(activity)
         alertDialogBuilder.setTitle(R.string.import_from_qr_code)
         binding = ConfigNamingDialogFragmentBinding.inflate(activity.layoutInflater, null, false)
@@ -72,37 +60,11 @@ class ConfigNamingDialogFragment : DialogFragment() {
             executePendingBindings()
             alertDialogBuilder.setView(root)
         }
-        alertDialogBuilder.setPositiveButton(R.string.create_tunnel, null)
+        alertDialogBuilder.setPositiveButton(R.string.create_tunnel) { _, _ -> createTunnelAndDismiss() }
         alertDialogBuilder.setNegativeButton(R.string.cancel) { _, _ -> dismiss() }
-        return alertDialogBuilder.create().apply {
-            setOnShowListener {
-                findViewById<TextInputEditText>(R.id.tunnel_name_text)?.apply {
-                    setOnFocusChangeListener { v, _ ->
-                        v.post {
-                            imm?.showSoftInput(v, InputMethodManager.SHOW_IMPLICIT)
-                        }
-                    }
-                    requestFocus()
-                }
-            }
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        val dialog = dialog as AlertDialog?
-        if (dialog != null) {
-            dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener { createTunnelAndDismiss() }
-            setKeyboardVisible(true)
-        }
-    }
-
-    private fun setKeyboardVisible(visible: Boolean) {
-        if (visible) {
-            imm!!.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
-        } else if (binding != null) {
-            imm!!.hideSoftInputFromWindow(binding!!.tunnelNameText.windowToken, 0)
-        }
+        val dialog = alertDialogBuilder.create()
+        dialog.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+        return dialog
     }
 
     companion object {
