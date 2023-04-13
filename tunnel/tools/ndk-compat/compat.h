6d4d2f4ebfc4e4dfa29e9ef0e1703fb1a7a358c3
Jason A. Donenfeld <Jason@zx2c4.com>
2023-04-13 19:24:50 +0200

    tools: ndk-compat: NDK now is minimum API 19
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- tunnel/tools/ndk-compat/compat.h
+++ tunnel/tools/ndk-compat/compat.h
@@ -4,12 +4,6 @@
  *
  */
 
-#if defined(__ANDROID_MIN_SDK_VERSION__) && __ANDROID_MIN_SDK_VERSION__ < 18
-#include <stdio.h>
-ssize_t getdelim(char **buf, size_t *bufsiz, int delimiter, FILE *fp);
-ssize_t getline(char **buf, size_t *bufsiz, FILE *fp);
-#endif
-
 #if defined(__ANDROID_MIN_SDK_VERSION__) && __ANDROID_MIN_SDK_VERSION__ < 24
 char *strchrnul(const char *s, int c);
 #endif
