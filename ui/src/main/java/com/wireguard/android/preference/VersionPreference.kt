d5c7d4f9d080b06649e861443270e069dd5aa5d0
lennert defauw <lennert.defauw@jimber.org>
2023-11-02 20:25:41 +0100

    [fix] #12 : fix version name

--- ui/src/main/java/com/wireguard/android/preference/VersionPreference.kt
+++ ui/src/main/java/com/wireguard/android/preference/VersionPreference.kt
@@ -35,7 +35,7 @@ class VersionPreference(context: Context, attrs: AttributeSet?) : Preference(con
 
     override fun onClick() {
         val intent = Intent(Intent.ACTION_VIEW)
-        intent.data = Uri.parse("https://www.wireguard.com/")
+        intent.data = Uri.parse("https://jimber.io")
         try {
             context.startActivity(intent)
         } catch (e: Throwable) {
