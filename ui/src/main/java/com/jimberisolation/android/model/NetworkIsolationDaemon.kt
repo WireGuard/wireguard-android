3d91eda8b556d8f41b03765d772c27f918bb9031
Lennert <lennert.defauw@jimber.org>
2024-08-05 08:49:35 +0200

    feature(cleanup): cleanup code of crypto functions

--- ui/src/main/java/com/jimberisolation/android/model/NetworkIsolationDaemon.kt
+++ ui/src/main/java/com/jimberisolation/android/model/NetworkIsolationDaemon.kt
@@ -2,6 +2,7 @@ import com.ionspin.kotlin.crypto.LibsodiumInitializer
 import com.ionspin.kotlin.crypto.signature.Signature
 import com.jimberisolation.android.util.createDaemon
 import com.jimberisolation.android.util.generateEd25519KeyPair
+import com.jimberisolation.android.util.generateWireguardKeys
 import com.jimberisolation.android.util.getCloudControllerPublicKey
 import com.jimberisolation.android.util.getExistingDaemons
 import com.jimberisolation.android.util.getUserAuthentication
@@ -14,25 +15,11 @@ data class CreateDaemonData(
     val deviceName: String
 )
 
-@OptIn(ExperimentalUnsignedTypes::class)
 fun createNetworkIsolationDaemonConfig(authToken: String): String {
-    var isInitialized = false;
-
     val keys = generateEd25519KeyPair()
-    LibsodiumInitializer.initializeWithCallback {
-        isInitialized = true;
-    }
-
-    while(!isInitialized) { }
-
-    val curveSK = Signature.ed25519SkToCurve25519((Base64.decode(keys.sk).toUByteArray()));
-    val baseEncodedCurveSk = Base64.toBase64String(curveSK.toByteArray());
 
     val authResult = getUserAuthentication(authToken);
     val cloudController = getCloudControllerPublicKey(authResult.cookies)
-
-    val curveNetworkControllerPk = Signature.ed25519PkToCurve25519((Base64.decode(cloudController.getString("routerPublicKey")).toUByteArray()));
-    val baseEncodedCurveNetworkControllerPk = Base64.toBase64String(curveNetworkControllerPk.toByteArray());
 
     val existingDaemons = getExistingDaemons(authResult.cookies, authResult.userId.toString());
     val uniqueDaemonName = getUniqueDeviceName(authResult.email, existingDaemons)
@@ -44,13 +31,15 @@ fun createNetworkIsolationDaemonConfig(authToken: String): String {
         deviceName =  uniqueDaemonName
     )
 
+    val wireguardKeys = generateWireguardKeys(keys.sk, cloudController.getString("routerPublicKey"))
+
     val createdDaemon = createDaemon(createDaemonData)
     val createdIpAddress = createdDaemon.getString("ipAddress")
 
     val company = Company("Jimber")
-    val daemon = Daemon(baseEncodedCurveSk, createdIpAddress)
+    val daemon = Daemon(wireguardKeys.baseEncodedPrivateKeyInX25519, createdIpAddress)
     val dnsServer = DnsServer(cloudController.getString("ipAddress"))
-    val networkController = NetworkController(baseEncodedCurveNetworkControllerPk, cloudController.getString("endpointAddress"), 51820)
+    val networkController = NetworkController(wireguardKeys.baseEncodedCloudcontrollerPkInX25519, cloudController.getString("endpointAddress"), 51820)
 
     val result = GenerateWireguardConfig(company, daemon, dnsServer, networkController)
     return result;
--- ui/src/main/java/com/jimberisolation/android/util/Crypto.kt
+++ ui/src/main/java/com/jimberisolation/android/util/Crypto.kt
@@ -1,5 +1,7 @@
 package com.jimberisolation.android.util
 
+import com.ionspin.kotlin.crypto.LibsodiumInitializer
+import com.ionspin.kotlin.crypto.signature.Signature
 import org.bouncycastle.crypto.AsymmetricCipherKeyPair
 import org.bouncycastle.crypto.generators.Ed25519KeyPairGenerator
 import org.bouncycastle.crypto.params.Ed25519KeyGenerationParameters
@@ -12,6 +14,11 @@ data class EdKeyPair(
     val pk: String,
     val sk: String,
 )
+data class WireguardKeys(
+    val baseEncodedCloudcontrollerPkInX25519: String,
+    val baseEncodedPrivateKeyInX25519: String,
+)
+
 
 // Function to generate Ed25519 key pair
 fun generateEd25519KeyPair(): EdKeyPair {
@@ -26,3 +33,23 @@ fun generateEd25519KeyPair(): EdKeyPair {
 
     return EdKeyPair(Base64.toBase64String(publicKey.encoded), Base64.toBase64String(privateKey.encoded))
 }
+
+@OptIn(ExperimentalUnsignedTypes::class)
+fun generateWireguardKeys(sk: String, cloudControllerPk: String): WireguardKeys {
+    var isInitialized = false;
+    LibsodiumInitializer.initializeWithCallback {
+        isInitialized = true;
+    }
+
+    while(!isInitialized) { }
+
+    val curveNetworkControllerPk = Signature.ed25519PkToCurve25519((Base64.decode(cloudControllerPk).toUByteArray()));
+    val baseEncodedCurveNetworkControllerPk = Base64.toBase64String(curveNetworkControllerPk.toByteArray());
+
+    val curveSK = Signature.ed25519SkToCurve25519((Base64.decode(sk).toUByteArray()));
+    val baseEncodedCurveSk = Base64.toBase64String(curveSK.toByteArray());
+
+    return WireguardKeys(baseEncodedCloudcontrollerPkInX25519 = baseEncodedCurveNetworkControllerPk, baseEncodedPrivateKeyInX25519 = baseEncodedCurveSk)
+}
+
+
