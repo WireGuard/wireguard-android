c98a3d3f49062977b5bb52e4b1962c0d6a55be7b
Lennert <lennert.defauw@jimber.org>
2024-10-22 13:18:59 +0200

    feature(auth): last fixes to prod

--- ui/src/main/java/com/jimberisolation/android/configStore/Config.kt
+++ ui/src/main/java/com/jimberisolation/android/configStore/Config.kt
@@ -1,6 +1,6 @@
 class Config {
     companion object {
-        const val BASE_URL = "https://signal.staging.jimber.io/api/v1/"
+        const val BASE_URL = "https://signal.jimber.io/api/v1/"
         const val GOOGLE_AUTH_ID = "107269124183-7i5qr58qcfaar9u4fbeodcs43s61lmtm.apps.googleusercontent.com"
     }
 }
