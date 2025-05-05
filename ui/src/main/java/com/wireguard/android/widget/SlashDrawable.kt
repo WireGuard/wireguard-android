9c0bafad1784010073288f42bb65bdd8e2eb7784
Jason A. Donenfeld <Jason@zx2c4.com>
2025-05-05 16:00:40 +0200

    gradle: update kotlin to 2.1.20
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- ui/src/main/java/com/wireguard/android/widget/SlashDrawable.kt
+++ ui/src/main/java/com/wireguard/android/widget/SlashDrawable.kt
@@ -69,6 +69,7 @@ class SlashDrawable(private val mDrawable: Drawable) : Drawable() {
 
     override fun getIntrinsicWidth() = mDrawable.intrinsicWidth
 
+    @Deprecated("Deprecated in API level 29")
     override fun getOpacity() = PixelFormat.OPAQUE
 
     override fun onBoundsChange(bounds: Rect) {
